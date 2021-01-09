8dca98e5294df1a30f6e45af05fcd1546bef5707
uberballo <kasper.paavolainen@hotmail.com>
2021-01-09 13:01:12 +0200

    Add spaghetti react window

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,6 +1,10 @@
-import React,{ useState, useEffect} from 'react'
+import React,{ useState, useEffect, forwardRef} from 'react'
 import getProducts from '../services/productService';
 import { useParams } from 'react-router-dom'
+import {AutoSizer, List} from 'react-virtualized';
+import { FixedSizeGrid as Grid } from 'react-window';
+
+
 
 
 const Container = () => {
@@ -31,8 +35,75 @@ const Container = () => {
         }
     };
 
-    return(
+function rowRenderer({key, index, style}) {
+    return (
+      <div key={key} >
+          <ProductCard pro={products[index]}/>
+      </div>
+    );
+  }
+  const GUTTER_SIZE = 50;
+
+  const rows = 5
+  const Cell = ({ columnIndex, rowIndex, style }) => {
+      const i = rows*rowIndex + columnIndex
+      //<ProductCard pro={products[i]}/>
+      const pro = products[i]
+      if (pro){
+      return(
+    <div 
+    className={"GridItem"}
+
+    style={{
+        ...style,
+        left: style.left + GUTTER_SIZE,
+        top: style.top + GUTTER_SIZE,
+        width: style.width - GUTTER_SIZE,
+        height: style.height - GUTTER_SIZE,
+      }
+    }>
+                <p> {pro.Name} </p>
+                <p> {pro.Stock} </p>
+    </div>)}else{
+        return <div style={style}>
+            Loading...
+        </div>
+    }
+  }
+   
+/*
+ return(
             <div className='Container'>{productRow()}</div>
+    )
+    */
+   const innerElementType = forwardRef(({ style, ...rest }, ref) => (
+    <div
+      ref={ref}
+      style={{
+        ...style,
+        paddingLeft: GUTTER_SIZE,
+        paddingTop: GUTTER_SIZE
+      }}
+      {...rest}
+    />
+  ));
+  
+ return(
+
+            <div className='Container'>
+    <Grid
+    columnCount={5}
+    columnWidth={250}
+    height={500}
+        innerElementType={innerElementType}
+
+    rowCount={100}
+    rowHeight={100}
+    width={1000}
+  >
+    {Cell}
+  </Grid>
+  </div>
     )
 }
 
--- server/server.go
+++ server/server.go
@@ -1,20 +1,16 @@
 package server
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
-	"github.com/go-co-op/gocron"
 	"github.com/uberballo/webstore/routes"
-	"github.com/uberballo/webstore/services/product"
 )
 
 func Start() {
 	router := gin.Default()
 	routes.Routes(router)
-	s1 := gocron.NewScheduler(time.UTC)
-	s1.Every(50).Seconds().Do(product.InitializeProductData)
-	s1.StartAsync()
+	//s1 := gocron.NewScheduler(time.UTC)
+	//s1.Every(50).Seconds().Do(product.InitializeProductData)
+	//s1.StartAsync()
 	// Start and run the server
 	router.Run(":5000")
 }
