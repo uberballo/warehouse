7ae2ab25526297274dc130a5a1978beea423ca89
kasper <kasper.paavolainen@hotmail.com>
2021-01-12 18:20:42 +0200

    Ignore requests.http and increate timeout limit

--- server/helpers/apihelper/request.go
+++ server/helpers/apihelper/request.go
@@ -8,6 +8,6 @@ import (
 //GetHTTPClient creates a HTTP client with timeout
 func GetHTTPClient() *http.Client {
 	return &http.Client{
-		Timeout: time.Second * 20,
+		Timeout: time.Second * 30,
 	}
 }
